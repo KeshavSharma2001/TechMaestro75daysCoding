aa3f5cf3007cbe20097015558198ca05e933daf3
Keshav Sharma <keshav292001@gmail.com>
2022-03-25 20:24:50 +0530

    Buy sell optimal

--- Day1/Best_Time_to_Buy_and_Sell_Stock.java
+++ Day1/Best_Time_to_Buy_and_Sell_Stock.java
@@ -2,39 +2,24 @@ package com.Keshav.Day1;
 
 public class Best_Time_to_Buy_and_Sell_Stock {
     public static int maxProfit(int[] prices) {
-        int min = prices[0];
+        int min_price = prices[0];
+        int profit = 0;
         for (int i = 0; i < prices.length; i++) {
-            if(prices[i] <min)
-                min = prices[i];
+//            if we have a lesser price for the stock
+            if(prices[i] < min_price) {
+                min_price = prices[i];
+            }
+//            difference of current price of stock with minimum price and compare with profit --> to have max profit
+            else if(prices[i] - min_price > profit){
+                profit = prices[i] - min_price;
+            }
         }
-        int min_idx = findIndex(min, prices);
-
-        int max = prices[0];
-        for (int i = 0; i < prices.length; i++) {
-            if(prices[i] > max)
-                max = prices[i];
-        }
-        int max_idx = findIndex(max, prices);
-
-        if(min_idx < max_idx){
-            int profit = prices[max_idx] - prices[min_idx];
-            return profit;
-        }
-        return 0;
-    }
-
-    public static int findIndex(int min, int[] prices) {
-        for (int i = 0; i < prices.length; i++) {
-            if(prices[i] == min)
-                return i;
-        }
-        return -1;
+        return profit;
     }
 
     public static void main(String[] args) {
-        int[] prices = {7,1,5,3,6,4};
+        int[] prices = {1,0,0,0,5};
         int profit = maxProfit(prices);
         System.out.println(profit);
-
     }
 }
